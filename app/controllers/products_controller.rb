0120ceedc6111a59412d3213d71e3685c94198ae
David Karpay <dlkarpay@gmail.com>
2013-10-08 17:55:42 -0400

    betastore app first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,5 @@
+class ProductsController < ApplicationController
+  def index
+    @page_title = "Products"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betastore::Application.config.secret_key_base = '8283942917aaee23ea8ca7b6360677657de0f13982226958e6c91d480c91c6ea4c076ddd12106fca2608d01cf6ba07198f8703b5eb93627ab5e93b3d8b7f1acd'
