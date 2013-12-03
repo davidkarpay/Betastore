b7780c9f31bbe4b349f3d7042c82670309da8f4d
David Karpay <dlkarpay@gmail.com>
2013-12-03 18:27:50 -0500

    workin on stripe

---
+++ db/migrate/20131203232337_add_stripe_fields_to_credit_cards.rb
@@ -0,0 +1,14 @@
+class AddStripeFieldsToCreditCards < ActiveRecord::Migration
+  def change
+    change_table :credit_cards do |t|
+      t.remove :card_number
+      t.remove :card_holder_name
+
+      t.integer :customer_id
+      t.string :stripe_token
+      t.string :card_type
+      t.string :last_4
+
+    end
+  end
+end
